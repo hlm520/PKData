4274e585b891e5736c8d7b60cc2b6b7e1d443aab
ThreeWater <649676485@qq.com>
2017-04-15 23:14:12 +0800

    Update and rename FileManager.java to FileFactory.java

---
+++ src/com/hlm520/FileFactory.java
@@ -0,0 +1,11 @@
+package com.hlm520;
+/**
+ * This is to extends GoalIndex.class 
+ * and you can creat a new element by this
+ *
+ /
+public class FileFactory extends GoalIndex
+{
+ //There is to do nothing ,
+ //but you can do everything after create this element.
+}
--- src/com/hlm520/FileManager.java
+++
@@ -1,8 +0,0 @@
-package com.hlm520;
-
-public class FileManager extends SetPath
-{
- 
- public FileManager(){}
- 
-}
